c94c610bcbb7aebf1f3e6f4f8aebf592de69e260
aloctavodia <aloctavodia@gmail.com>
2025-02-11 10:38:45 +0200

    add isotonic_fit

--- src/arviz_stats/helper_stats.py
+++ src/arviz_stats/helper_stats.py
@@ -1,18 +1,14 @@
-"""
-Auxiliary statistical functions
-"""
+"""Auxiliary statistical functions."""
 
 import numpy as np
-from scipy.optimize import isotonic_regression
 from scipy import interpolate
-
-from arviz_base import extract, rcParams
+from scipy.optimize import isotonic_regression
 
 
 def isotonic_fit(pred, obs, n_bootstrap, ci_prob=None):
     """
     Perform isotonic regression on the observed data and return the fitted values.
-    
+
     Parameters
     ----------
     pred : array-like
@@ -26,6 +22,7 @@ def isotonic_fit(pred, obs, n_bootstrap, ci_prob=None):
     """
     pred, obs = _sort_pred_with_obs(pred, obs)
     y_out = isotonic_regression(obs, increasing=True).x
+    rng = np.random.default_rng(42)
 
     # calculate the fitting function
     unique_pred_sorted, pred_counts = np.unique(pred, return_counts=True)
@@ -35,12 +32,13 @@ def isotonic_fit(pred, obs, n_bootstrap, ci_prob=None):
     # bootstrap the isotonic regression
     result = np.zeros((n_bootstrap, len(pred_counts)))
     for i in range(n_bootstrap):
-        idx = np.random.choice(len(pred), len(pred), replace=True)
+        idx = rng.choice(len(pred), len(pred), replace=True)
         pred_boot, obs_boot = _sort_pred_with_obs(pred[idx], obs[idx])
         y_out_boot = isotonic_regression(obs_boot, increasing=True).x
 
-        result[i] = interpolate.interp1d(pred_boot, y_out_boot, bounds_error=False)(unique_pred_sorted)
-
+        result[i] = interpolate.interp1d(pred_boot, y_out_boot, bounds_error=False)(
+            unique_pred_sorted
+        )
 
     # we should compute hdi or eti using already implemented functions
     lb = (1 - ci_prob) / 2 * 100
@@ -50,9 +48,8 @@ def isotonic_fit(pred, obs, n_bootstrap, ci_prob=None):
     return regression_values, unique_pred_sorted, regression_values_boot
 
 
-
 def _sort_pred_with_obs(pred, obs):
-    """ Ensure that ties in pred have same regression value in PAV algorithm"""
+    """Ensure ties in pred have same regression value in PAV algorithm."""
     sorter = np.lexsort((-obs, pred))
     pred = pred[sorter]
     obs = obs[sorter]
