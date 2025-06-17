b4a9482bcb88471394eb60da6bf40ac685c89244
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-06-17 18:18:49 -0400

    test: fix test by raising attribute error

--- src/arviz_stats/loo/wrapper.py
+++ src/arviz_stats/loo/wrapper.py
@@ -189,7 +189,7 @@ class SamplingWrapper:
             getattr(self, method)(*args)
         except NotImplementedError:
             return False
-        except (TypeError, ValueError, RuntimeError):
+        except (TypeError, ValueError, RuntimeError, AttributeError):
             return True
         return True
 
