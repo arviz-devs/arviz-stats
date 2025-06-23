ef3f40b276789f7ec1563a17755a57299b8e5959
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-06-23 18:46:41 -0400

    fix: update reloo and add multidim tests

--- src/arviz_stats/loo/reloo.py
+++ src/arviz_stats/loo/reloo.py
@@ -17,12 +17,12 @@ __all__ = ["reloo"]
 def reloo(
     wrapper,
     loo_orig=None,
-    k_threshold=0.7,
+    k_threshold=-np.inf,
     pointwise=None,
 ):
     r"""Recalculate exact Leave-One-Out cross validation refitting where the approximation fails.
 
-    ``arviz_stats.loo.loo`` estimates the values of Leave-One-Out (LOO) cross validation using
+    :func:`arviz_stats.loo` estimates the values of Leave-One-Out (LOO) cross validation using
     Pareto Smoothed Importance Sampling (PSIS) to approximate its value. PSIS works well when
     the posterior and the posterior_i (excluding observation i from the data used to fit)
     are similar. In some cases, there are highly influential observations for which PSIS
@@ -47,9 +47,10 @@ def reloo(
     loo_orig : ELPDData, optional
         Existing LOO results with pointwise data. If None, will compute
         PSIS-LOO-CV first using the data from ``wrapper``.
-    k_threshold : float, default 0.7
+    k_threshold : float, optional
         Pareto shape threshold. Observations with k values above this
-        threshold will trigger a refit.
+        threshold will trigger a refit. Defaults to :math:`\min(1 - 1/\log_{10}(S), 0.7)`,
+        where S is the number of samples.
     pointwise : bool, optional
         If True, return pointwise LOO data. Defaults to
         ``rcParams["stats.ic_pointwise"]``.
@@ -62,7 +63,7 @@ def reloo(
 
     Notes
     -----
-    It is strongly recommended to first compute ``arviz_stats.loo.loo`` on the inference results to
+    It is strongly recommended to first compute :func:`arviz_stats.loo` on the inference results to
     confirm that the number of values above the threshold is small enough. Otherwise,
     prohibitive computation time may be needed to perform all required refits.
 
@@ -70,6 +71,12 @@ def reloo(
     larger than the threshold would make ``reloo`` perform the whole exact LOO-CV.
     This is not generally recommended nor intended, however, if needed, this function can
     be used to achieve the result.
+
+    The term `exact cross-validation` is used here to refer to the process of refitting the model
+    for each observation, which is also commonly called `brute-force cross-validation`. While
+    this eliminates the PSIS approximation, other approximations remain in the process, such as
+    those inherent in the fitting procedure itself and the use of cross-validation as a proxy
+    for expected log predictive density (ELPD).
 
     Warnings
     --------
@@ -128,12 +135,17 @@ def reloo(
     loo_inputs = _prepare_loo_inputs(wrapper.data, wrapper.log_lik_var_name)
     obs_dims = loo_inputs.obs_dims
     n_data_points = loo_inputs.n_data_points
+    n_samples = loo_inputs.n_samples
+
+    if k_threshold is None:
+        k_threshold = min(1 - 1 / np.log10(n_samples), 0.7)
 
     pareto_k_stacked = loo_orig.pareto_k.stack(__obs__=obs_dims).transpose("__obs__")
     bad_k_mask = pareto_k_stacked > k_threshold
-    bad_obs_indices = np.where(bad_k_mask.values)[0]
 
-    if len(bad_obs_indices) == 0:
+    bad_obs_flat_indices = np.where(bad_k_mask.values)[0]
+
+    if len(bad_obs_flat_indices) == 0:
         if not pointwise:
             loo_refitted.elpd_i = None
             loo_refitted.pareto_k = None
@@ -141,14 +153,33 @@ def reloo(
 
     lppd_orig = loo_orig.p + loo_orig.elpd
 
-    for obs_idx in bad_obs_indices:
-        new_obs, excluded_obs = wrapper.sel_observations(obs_idx)
+    if len(obs_dims) == 1:
+        bad_obs_indices = bad_obs_flat_indices
+    else:
+        obs_shape = [len(loo_orig.pareto_k.coords[dim]) for dim in obs_dims]
+        bad_obs_indices = np.array(np.unravel_index(bad_obs_flat_indices, obs_shape)).T
+
+    for i, _ in enumerate(bad_obs_flat_indices):
+        if len(obs_dims) == 1:
+            obs_idx = bad_obs_indices[i]
+            coord_value = loo_orig.pareto_k.coords[obs_dims[0]].values[obs_idx]
+            obs_idx_dict = {obs_dims[0]: coord_value}
+            sel_idx = obs_idx
+        else:
+            obs_idx = bad_obs_indices[i]
+            obs_idx_dict = {}
+            for j, dim in enumerate(obs_dims):
+                coord_value = loo_orig.pareto_k.coords[dim].values[obs_idx[j]]
+                obs_idx_dict[dim] = coord_value
+            sel_idx = obs_idx_dict
+
+        new_obs, excluded_obs = wrapper.sel_observations(sel_idx)
         fit = wrapper.sample(new_obs)
         idata_idx = wrapper.get_inference_data(fit)
         log_lik_idx = wrapper.log_likelihood__i(excluded_obs, idata_idx)
         loo_lppd_idx = logsumexp(log_lik_idx, dims=sample_dims, b=1 / log_lik_idx.size).item()
-        loo_refitted.elpd_i.loc[obs_idx] = loo_lppd_idx
-        loo_refitted.pareto_k.loc[obs_idx] = 0.0
+        loo_refitted.elpd_i.loc[obs_idx_dict] = loo_lppd_idx
+        loo_refitted.pareto_k.loc[obs_idx_dict] = 0.0
 
     loo_refitted.elpd = np.sum(loo_refitted.elpd_i.values)
     loo_refitted.se = np.sqrt(n_data_points * np.var(loo_refitted.elpd_i.values))
