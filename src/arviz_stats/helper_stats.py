1f8ad0fff904797d4416e4bb7412841935ed1354
Osvaldo A Martin <aloctavodia@gmail.com>
2025-08-06 11:18:12 +0300

    compute isotonic regression for categorical and ordinal data (#167)

--- src/arviz_stats/helper_stats.py
+++ src/arviz_stats/helper_stats.py
@@ -1,43 +1,100 @@
 """Auxiliary statistical functions."""
 
+import warnings
+
 import numpy as np
 from arviz_base import dict_to_dataset, extract
 from scipy import interpolate
 from scipy.optimize import isotonic_regression
 
 
-def isotonic_fit(dt, data_pairs, group, n_bootstrap, ci_prob):
+def isotonic_fit(dt, var_names, group, n_bootstrap, ci_prob, data_type="binary"):
     """
     Perform isotonic regression over a DataTree.
+
+    This is helpful to create calibration plots for binary, categorical, or ordinal regression.
+    Isotonic regression can also be used for continuous data, but it is less common.
 
     Parameters
     ----------
     dt: DataTree
         DataTree with "posterior_predictive" and "observed_data" groups
-    data_pairs : dict
-        Dictionary of keys prior/posterior predictive data and values observed data variable names.
+    var_names : str or list of str, optional
+        The variables to perform the isotonic regression on.
     group : str
         The group from which to get the unique values.
     n_bootstrap : int
         The number of bootstrap samples to use.
     ci_prob : float, optional
         The probability for the credible interval.
+    data_type : str
+        Defaults to "binary", other options are "categorical" and "ordinal".
     """
     pp = extract(dt, group=group, keep_dataset=True)
-
     dictio = {}
-    vars_ = []
 
-    if None in data_pairs.keys():
-        data_pairs = dict(zip(dt[group].data_vars, dt.observed_data.data_vars))
+    if var_names is None:
+        var_names = dt.observed_data.data_vars
 
-    for var_predictive, var_obs in data_pairs.items():
-        pred = pp[var_predictive].mean("sample")
-        cep, forecasted, ci_lb, ci_ub = _isotonic_fit(
-            pred, dt.observed_data[var_obs], n_bootstrap, ci_prob
-        )
-        dictio[var_predictive] = np.stack([forecasted, cep, ci_lb, ci_ub])
-        vars_.append(var_predictive)
+    if isinstance(var_names, str):
+        var_names = [var_names]
+
+    valid_data_types = ["binary", "categorical", "ordinal"]
+    if data_type not in valid_data_types:
+        raise ValueError(f"Invalid data_type '{data_type}'. Must be one of {valid_data_types}.")
+
+    for v_name in var_names:
+        preds = pp[v_name]
+        observed = dt.observed_data[v_name]
+
+        if data_type == "binary":
+            pred_mean = preds.mean("sample")
+            cep, forecasted, ci_lb, ci_ub = _isotonic_fit(pred_mean, observed, n_bootstrap, ci_prob)
+            dictio[v_name] = np.stack([forecasted, cep, ci_lb, ci_ub])
+
+        elif data_type in ("categorical", "ordinal"):
+            obs_vals = observed.values
+            if np.issubdtype(obs_vals.dtype, np.floating):
+                warnings.warn(
+                    f"Kind '{data_type}' specified but data for '{v_name}' appears to be "
+                    "continuous. Isotonic regression can be used continuous data, but is not "
+                    "the most common use case. If that's the case set `kind=binary`.",
+                    UserWarning,
+                )
+            elif np.unique(obs_vals).size <= 2:
+                warnings.warn(
+                    f"Kind '{data_type}' specified but data for '{v_name}' appears to be binary. "
+                    "setting `kind=binary` may be more appropriate.",
+                    UserWarning,
+                )
+            n_categories = int(np.max([preds.max(), observed.max()])) + 1
+            prob_per_cat = [(preds == k).mean("sample") for k in range(n_categories)]
+            prob_per_cat = np.stack(prob_per_cat, axis=-1)
+
+            if data_type == "categorical":
+                # for categorical data, we need to handle each category separately
+                # and compare each category against the rest (one vs others)
+                obs_one_hot = np.eye(n_categories)[obs_vals]
+
+                for k in range(n_categories):
+                    cep, forecasted, ci_lb, ci_ub = _isotonic_fit(
+                        prob_per_cat[..., k], obs_one_hot[..., k], n_bootstrap, ci_prob
+                    )
+                    dictio[f"{v_name} {k} vs others"] = np.stack([forecasted, cep, ci_lb, ci_ub])
+
+            else:
+                # for ordinal data, we need to respect the order so we compute the
+                # cumulative probabilities
+                cum_probs = np.cumsum(prob_per_cat, axis=-1)
+                cum_indicators = (np.expand_dims(obs_vals, -1) <= np.arange(n_categories)).astype(
+                    int
+                )
+
+                for k in range(n_categories - 1):
+                    cep, forecasted, ci_lb, ci_ub = _isotonic_fit(
+                        cum_probs[..., k], cum_indicators[..., k], n_bootstrap, ci_prob
+                    )
+                    dictio[f"{v_name} cumul≤{k}"] = np.stack([forecasted, cep, ci_lb, ci_ub])
 
     return (
         dict_to_dataset(dictio)
