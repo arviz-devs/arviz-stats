3e80e81157ec7ab8fa3e97ba4c5c88422ea6aeda
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2025-08-08 01:16:39 +0200

    improve docs

--- src/arviz_stats/visualization.py
+++ src/arviz_stats/visualization.py
@@ -97,7 +97,7 @@ def hdi(
 
     .. ipython::
 
-        In [1]: azs.hdi(data, dim=["chain","draw", "school"])
+        In [1]: azs.hdi(dt, dim=["chain","draw", "school"])
     """
     prob = validate_ci_prob(prob)
     return _apply_multi_input_function(
@@ -188,7 +188,7 @@ def eti(
         In [1]: import arviz_stats as azs
            ...: import numpy as np
            ...: data = np.random.default_rng().normal(size=2000)
-           ...: azs.eti(data, hdi_prob=.68)
+           ...: azs.eti(data, 0.68)
 
     Calculate the ETI for specific variables:
 
@@ -202,7 +202,7 @@ def eti(
 
     .. ipython::
 
-        In [1]: azs.hdi(data, dim=["chain","draw", "school"])
+        In [1]: azs.eti(dt, dim=["chain","draw", "school"])
     """
     prob = validate_ci_prob(prob)
     return _apply_multi_input_function(
@@ -231,9 +231,76 @@ def ecdf(
     pit=False,
     **kwargs,
 ):
-    """Compute ECDF.
+    r"""Compute the marginal empirical cumulative density functions (ECDF).
 
-    TODO: add full detailed docstring that can be referenced from accessors and da/array interfaces.
+    See the EABM chapter on `Visualization of Random Variables with ArviZ <https://arviz-devs.github.io/EABM/Chapters/Distributions.html#distributions-in-arviz>`_
+    for more details.
+
+    Parameters
+    ----------
+    data : array-like, DataArray, Dataset, DataTree, DataArrayGroupBy, DatasetGroupBy, or idata-like
+        Input data. It will have different pre-processing applied to it depending on its type:
+
+        - array-like: call array layer within ``arviz-stats``.
+        - xarray object: apply dimension aware function to all relevant subsets
+        - others: passed to :func:`arviz_base.convert_to_dataset` then treated as
+          :class:`xarray.Dataset`. This option is discouraged due to needing this conversion
+          which is completely automated and will be needed again in future executions or
+          similar functions.
+
+          It is recommeded to first perform the conversion manually and then call
+          ``arviz_stats.hdi``. This allows controlling the conversion step and inspecting
+          its results.
+    dim : sequence of hashable, optional
+        Dimensions to be reduced when computing the ECDF. Default ``rcParams["data.sample_dims"]``.
+    group : hashable, default "posterior"
+        Group on which to compute the ECDF
+    var_names : str or list of str, optional
+        Names of the variables for which the ECDF should be computed.
+    filter_vars : {None, "like", "regex"}, default None
+    coords : dict, optional
+        Dictionary of dimension/index names to coordinate values defining a subset
+        of the data for which to perform the computation.
+    pit : bool, default False
+    **kwargs : any, optional
+        Forwarded to the array or dataarray interface for ECDF.
+
+    Returns
+    -------
+    ndarray, DataArray, Dataset, DataTree
+        Requested HDI of the provided input.
+        It will have a ``quantile`` dimension and a ``plot_axis`` dimension with coordinate
+        values "x" and "y".
+
+    See Also
+    --------
+    arviz_stats.kde, arviz_stats.histogram : Alternative visual summaries for marginal distributions
+    arviz_plots.plot_dist
+
+    Examples
+    --------
+    Calculate the ECDF of a Normal random variable:
+
+    .. ipython::
+
+        In [1]: import arviz_stats as azs
+           ...: import numpy as np
+           ...: data = np.random.default_rng().normal(size=2000)
+           ...: # not available yet in array interface azs.ecdf(data)
+
+    Calculate the ECDF for specific variables:
+
+    .. ipython::
+
+        In [1]: import arviz_base as azb
+           ...: dt = azb.load_arviz_data("centered_eight")
+           ...: azs.ecdf(dt.posterior.dataset, var_names=["mu", "theta"])
+
+    Calculate the ECDF also over the school dimension (for variables where present):
+
+    .. ipython::
+
+        In [1]: azs.ecdf(dt.posterior.dataset, dim=["chain", "draw", "school"])
     """
     return _apply_multi_input_function(
         "ecdf",
@@ -261,9 +328,80 @@ def histogram(
     weights=None,
     density=True,
 ):
-    """Compute histogram.
+    r"""Compute the batched histogram.
 
-    TODO: add full detailed docstring that can be referenced from accessors and da/array interfaces.
+    See the EABM chapter on `Visualization of Random Variables with ArviZ <https://arviz-devs.github.io/EABM/Chapters/Distributions.html#distributions-in-arviz>`_
+    for more details.
+
+    Parameters
+    ----------
+    data : array-like, DataArray, Dataset, DataTree, DataArrayGroupBy, DatasetGroupBy, or idata-like
+        Input data. It will have different pre-processing applied to it depending on its type:
+
+        - array-like: call array layer within ``arviz-stats``.
+        - xarray object: apply dimension aware function to all relevant subsets
+        - others: passed to :func:`arviz_base.convert_to_dataset` then treated as
+          :class:`xarray.Dataset`. This option is discouraged due to needing this conversion
+          which is completely automated and will be needed again in future executions or
+          similar functions.
+
+          It is recommeded to first perform the conversion manually and then call
+          ``arviz_stats.hdi``. This allows controlling the conversion step and inspecting
+          its results.
+    dim : sequence of hashable, optional
+        Dimensions to be reduced when computing the histogram.
+        Default ``rcParams["data.sample_dims"]``.
+    group : hashable, default "posterior"
+        Group on which to compute the histogram
+    var_names : str or list of str, optional
+        Names of the variables for which the histogram should be computed.
+    filter_vars : {None, "like", "regex"}, default None
+    coords : dict, optional
+        Dictionary of dimension/index names to coordinate values defining a subset
+        of the data for which to perform the computation.
+    bind : array-like, optional
+    range : array-like, optional
+    weights : array-like, optional
+    density : bool, default True
+    **kwargs : any, optional
+        Forwarded to the array or dataarray interface for histogram.
+
+    Returns
+    -------
+    ndarray, DataArray, Dataset, DataTree
+        Requested HDI of the provided input.
+        It will have a ``{var_name}_hist_dim`` dimension and a ``plot_axis`` dimension
+        with coordinates "histogram", "left_edges" and "right_edges"
+
+    See Also
+    --------
+    arviz_stats.ecdf, arviz_stats.kde : Alternative visual summaries for marginal distributions
+    arviz_plots.plot_dist
+
+    Examples
+    --------
+    Calculate the histogram of a Normal random variable:
+
+    .. ipython::
+
+        In [1]: import arviz_stats as azs
+           ...: import numpy as np
+           ...: data = np.random.default_rng().normal(size=2000)
+           ...: azs.histogram(data)
+
+    Calculate the histogram for specific variables:
+
+    .. ipython::
+
+        In [1]: import arviz_base as azb
+           ...: dt = azb.load_arviz_data("centered_eight")
+           ...: azs.histogram(dt, var_names=["mu", "theta"])
+
+    Calculate the histogram also over the school dimension (for variables where present):
+
+    .. ipython::
+
+        In [1]: azs.histogram(dt, dim=["chain", "draw", "school"])
     """
     return _apply_multi_input_function(
         "histogram",
@@ -291,9 +429,78 @@ def kde(
     circular=False,
     **kwargs,
 ):
-    """Compute KDE.
+    r"""Compute the marginal kernel density estimates (KDE).
 
-    TODO: add full detailed docstring that can be referenced from accessors and da/array interfaces.
+    See the EABM chapter on `Visualization of Random Variables with ArviZ <https://arviz-devs.github.io/EABM/Chapters/Distributions.html#distributions-in-arviz>`_
+    for more details.
+
+    Parameters
+    ----------
+    data : array-like, DataArray, Dataset, DataTree, DataArrayGroupBy, DatasetGroupBy, or idata-like
+        Input data. It will have different pre-processing applied to it depending on its type:
+
+        - array-like: call array layer within ``arviz-stats``.
+        - xarray object: apply dimension aware function to all relevant subsets
+        - others: passed to :func:`arviz_base.convert_to_dataset` then treated as
+          :class:`xarray.Dataset`. This option is discouraged due to needing this conversion
+          which is completely automated and will be needed again in future executions or
+          similar functions.
+
+          It is recommeded to first perform the conversion manually and then call
+          ``arviz_stats.hdi``. This allows controlling the conversion step and inspecting
+          its results.
+    dim : sequence of hashable, optional
+        Dimensions to be reduced when computing the KDE.
+        Default ``rcParams["data.sample_dims"]``.
+    group : hashable, default "posterior"
+        Group on which to compute the KDE
+    var_names : str or list of str, optional
+        Names of the variables for which the KDE should be computed.
+    filter_vars : {None, "like", "regex"}, default None
+    coords : dict, optional
+        Dictionary of dimension/index names to coordinate values defining a subset
+        of the data for which to perform the computation.
+    circular : bool, default False
+    **kwargs : any, optional
+        Forwarded to the array or dataarray interface for KDE.
+
+    Returns
+    -------
+    ndarray, DataArray, Dataset, DataTree
+        Requested HDI of the provided input.
+        It will have a ``{var_name}_hist_dim`` dimension and a ``plot_axis`` dimension
+        with coordinates "histogram", "left_edges" and "right_edges"
+
+    See Also
+    --------
+    arviz_stats.ecdf, arviz_stats.histogram :
+        Alternative visual summaries for marginal distributions
+    arviz_plots.plot_dist
+
+    Examples
+    --------
+    Calculate the KDE of a Normal random variable:
+
+    .. ipython::
+
+        In [1]: import arviz_stats as azs
+           ...: import numpy as np
+           ...: data = np.random.default_rng().normal(size=2000)
+           ...: azs.kde(data)
+
+    Calculate the KDE for specific variables:
+
+    .. ipython::
+
+        In [1]: import arviz_base as azb
+           ...: dt = azb.load_arviz_data("centered_eight")
+           ...: azs.kde(dt, var_names=["mu", "theta"])
+
+    Calculate the KDE also over the school dimension (for variables where present):
+
+    .. ipython::
+
+        In [1]: azs.kde(dt, dim=["chain", "draw", "school"])
     """
     return _apply_multi_input_function(
         "kde",
