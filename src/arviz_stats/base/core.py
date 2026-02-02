Fix bins computation (#295)
* fix bins computation

* make variable name and comments more clear

* ensure a width of at least 1 for integers, update test

src/arviz_stats/base/core.py
@@ -199,17 +199,20 @@ class _CoreBase:
                 width = max(1, width)
             return x_min, x_max, width
 
-        # Sturges histogram bin estimator
+        # Sturges width estimator
         width_sturges = (x_max - x_min) / (np.log2(values.size) + 1)
 
-        # The Freedman-Diaconis histogram bin estimator.
+        # The Freedman-Diaconis width estimator.
         iqr = np.subtract(*self.quantile(values, [0.75, 0.25]))  # pylint: disable=assignment-from-no-return
         width_fd = 2 * iqr * values.size ** (-1 / 3)
 
+        # Correct Freedman-Diaconis. Heuristic to limit the maximal number of bins
+        width_sqrt = (x_max - x_min) / np.sqrt(values.size)
+        corr_fd = max(width_fd, width_sqrt / 2)
+
+        width = min(width_sturges, corr_fd)
         if dtype == "i":
-            width = np.round(np.max([1, width_sturges, width_fd])).astype(int)
-        else:
-            width = np.max([width_sturges, width_fd])
+            width = np.round(max(1, width)).astype(int)
 
         return x_min, x_max, width
 
@@ -234,7 +237,7 @@ class _CoreBase:
 
         Notes
         -----
-        Computes the width of the bins by taking the maximum of the Sturges and
+        Computes the width of the bins by taking the minimum of the Sturges and
         the Freedman-Diaconis estimators.
         According to numpy `np.histogram` this provides good all around performance.
 